9932420f9962c3bb9f34601c5a292a143b6f79fe
dd0h <ddoh@o2.pl>
2021-01-24 21:48:02 +0100

    Responsive desing

--- public/views/view_parts/common/logged_menu.php
+++ public/views/view_parts/common/logged_menu.php
@@ -1,10 +1,10 @@
 <div class="logged-menu">
     <div class="forms">
         <form action="addRecipe" method="GET">
-            <button type="submit"><p>Add Recipe</p></button>
+            <button type="submit">Add Recipe</button>
         </form>
         <form action="logout" method="POST">
-            <button type="submit"><p>Logout</p></button>
+            <button type="submit">Logout</button>
         </form>
     </div>
     <div class="logged-as">Logged as: <?php if(isset($username)) echo $username ?></div>
